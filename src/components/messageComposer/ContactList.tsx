35085c91eaa6506187167ef374772adfd2f84920
dgrah50 <d.graham50@hotmail.co.uk>
2023-04-20 16:03:26 +0100

    less jerkiness on contact list

--- src/components/messageComposer/ContactList.tsx
+++ src/components/messageComposer/ContactList.tsx
@@ -7,6 +7,7 @@ import { Person } from '~/store/modalStore';
 import Image from 'next/image';
 import { isPerson } from './index';
 import { recent, people } from './mockData';
+import clsx from 'clsx';
 
 interface ContactListProps {
   onPersonClick: (value: Person) => void;
@@ -135,49 +136,54 @@ export function ContactList({ onPersonClick }: ContactListProps) {
                   )}
                 </div>
 
-                {activeOption && (
-                  <div className="hidden h-96 w-1/2 flex-none flex-col divide-y divide-gray-100 overflow-y-auto sm:flex">
-                    <div className="flex-none p-6 text-center">
-                      <Image
-                        src={activeOption.imageUrl}
-                        alt=""
-                        className="mx-auto h-16 w-16 rounded-full"
-                        width="24"
-                        height="24"
-                      />
-                      <h2 className="mt-3 font-semibold text-gray-900">{activeOption.name}</h2>
-                      <p className="text-sm leading-6 text-gray-500">{activeOption.role}</p>
-                    </div>
-                    <div className="flex flex-auto flex-col justify-between p-6">
-                      <dl className="grid grid-cols-1 gap-x-6 gap-y-3 text-sm text-gray-700">
-                        <dt className="col-end-1 font-semibold text-gray-900">Phone</dt>
-                        <dd>{activeOption.phone}</dd>
-                        <dt className="col-end-1 font-semibold text-gray-900">URL</dt>
-                        <dd className="truncate">
-                          <a href={activeOption.url} className="text-indigo-600 underline">
-                            {activeOption.url}
-                          </a>
-                        </dd>
-                        <dt className="col-end-1 font-semibold text-gray-900">Email</dt>
-                        <dd className="truncate">
-                          <a
-                            href={`mailto:${activeOption.email}`}
-                            className="text-indigo-600 underline"
-                          >
-                            {activeOption.email}
-                          </a>
-                        </dd>
-                      </dl>
-                      <button
-                        type="button"
-                        onClick={() => onPersonClick(activeOption)}
-                        className="mt-6 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                        Send message
-                      </button>
-                    </div>
+                <div
+                  className={clsx(
+                    'hidden h-96 w-1/2 flex-none flex-col divide-y divide-gray-100 overflow-y-auto sm:flex',
+                    {
+                      invisible: activeOption == null,
+                    },
+                  )}
+                >
+                  <div className="flex-none p-6 text-center">
+                    <Image
+                      src={activeOption?.imageUrl ?? ''}
+                      alt=""
+                      className="mx-auto h-16 w-16 rounded-full"
+                      width="24"
+                      height="24"
+                    />
+                    <h2 className="mt-3 font-semibold text-gray-900">{activeOption?.name}</h2>
+                    <p className="text-sm leading-6 text-gray-500">{activeOption?.role}</p>
                   </div>
-                )}
+                  <div className="flex flex-auto flex-col justify-between p-6">
+                    <dl className="grid grid-cols-1 gap-x-6 gap-y-3 text-sm text-gray-700">
+                      <dt className="col-end-1 font-semibold text-gray-900">Phone</dt>
+                      <dd>{activeOption?.phone}</dd>
+                      <dt className="col-end-1 font-semibold text-gray-900">URL</dt>
+                      <dd className="truncate">
+                        <a href={activeOption?.url} className="text-indigo-600 underline">
+                          {activeOption?.url}
+                        </a>
+                      </dd>
+                      <dt className="col-end-1 font-semibold text-gray-900">Email</dt>
+                      <dd className="truncate">
+                        <a
+                          href={`mailto:${activeOption?.email}`}
+                          className="text-indigo-600 underline"
+                        >
+                          {activeOption?.email}
+                        </a>
+                      </dd>
+                    </dl>
+                    <button
+                      type="button"
+                      onClick={() => activeOption != null && onPersonClick(activeOption)}
+                      className="mt-6 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Send message
+                    </button>
+                  </div>
+                </div>
               </>
             </Combobox.Options>
           )}
